d78542da6c5471fef3bd770f8cc8bff50a9f6848
Jose Felix <jfelx@outlook.com>
2020-10-22 17:57:58 -0400

    fix: incorrect typings

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,14 +4,14 @@ describe('withPrismaPlugin', () => {
   it('should keep nextConfig object properties in other phases', () => {
     const nextConfig = { foo: 'bar' }
 
-    const newConfig = withPrismaPlugin(nextConfig)('NO_PHASE', {})
+    const newConfig = withPrismaPlugin(nextConfig)('phase-export', {})
     expect(newConfig).toEqual(nextConfig)
   })
 
   it('should call nextConfig function in other phases', () => {
     const nextConfig = () => ({ foo: 'bar' })
 
-    const newConfig = withPrismaPlugin(nextConfig)('NO_PHASE', {})
+    const newConfig = withPrismaPlugin(nextConfig)('phase-export', {})
     expect(newConfig).toEqual(nextConfig())
   })
 
