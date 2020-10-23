79acdf32ace0c0e58d99606d28036c4eed39e9e6
William Luke <william@ordino.ai>
2020-10-23 12:06:35 +0300

    feat: enable custom output

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import path from 'path'
 import PrismaClientReloaderWebpackPlugin from './webpack-plugin'
+
 const PATH_DELIMITER = '[\\\\/]' // match 2 antislashes or one slash
 
 /**
@@ -6,45 +9,54 @@ const PATH_DELIMITER = '[\\\\/]' // match 2 antislashes or one slash
  * paths of the modules. So we need to check for \\ and /
  */
 // @ts-ignore
-const safePath = module => module.split(/[\\\/]/g).join(PATH_DELIMITER)
+const safePath = (module) => module.split(/[\\\/]/g).join(PATH_DELIMITER)
 
 /**
  * Actual Next.js plugin
  */
-
-const withPrismaPlugin = (nextConfig = {}) => (
-  phase:
-    | "phase-export"
-    | "phase-production-build"
-    | "phase-production-server"
-    | "phase-development-server",
-    thing: any
+const withPrismaPlugin = (nextConfig: any) => (
+  phase: 'phase-export' | 'phase-production-build' | 'phase-production-server' | 'phase-development-server',
+  ...thing: any
 ) => {
-  if (phase === "phase-development-server") {
-    return Object.assign({}, nextConfig, {
-      webpack(config: any, options: any) {
-        const ignore = ['.prisma/client', '@prisma/client']
-
-        // const includes = ignore.map(module => (new RegExp(`${module}(?!.*node_modules)`)));
-        const excludes = [new RegExp(`node_modules(?!/(${ignore.join('|')})(?!.*node_modules))`)];
-        const ignored = config.watchOptions.ignored.filter(
-          (ignored: string) => ignored !== '**/node_modules/**'
-        ).concat(excludes);
-        return Object.assign(config, {
-          plugins: [...config.plugins, new PrismaClientReloaderWebpackPlugin()],
-          watchOptions: {
-            ...config.watchOptions,
-            ignored
-          }
-        })
+  const config = { ...nextConfig }
+  config.webpack = (webpackConfig: any, options: any) => {
+    if (typeof nextConfig?.webpack === 'function') {
+      nextConfig?.webpack(webpackConfig, options)
+    }
+    config.node.__dirname = false
+    if (phase === 'phase-development-server') {
+      config.node.__dirname = true
+      const ignore = ['.prisma/client', '@prisma/client']
+      // const includes = ignore.map(module => (new RegExp(`${module}(?!.*node_modules)`)));
+      const excludes = [new RegExp(`node_modules(?!/(${ignore.join('|')})(?!.*node_modules))`)]
+      const ignored = webpackConfig.watchOptions.ignored
+        .filter((ignored: string) => ignored !== '**/node_modules/**')
+        .concat(excludes)
+      webpackConfig.watchOptions = {
+        ...webpackConfig.watchOptions,
+        ignored,
       }
-    })
+    }
+    // Custom Output Fix
+    webpackConfig.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            // the custom location the binary is generated to
+            from: '**/query-engine*', // glob pattern that matches all query-engines
+
+            to: path.join(__dirname, '.next/serverless/pages/api'),
+          },
+        ],
+      })
+    )
+    webpackConfig.plugins = [...webpackConfig.plugins, new PrismaClientReloaderWebpackPlugin()]
+    // set to false
+    // config.node.__dirname = false;
+    return webpackConfig
   }
-  let internalConfigObj =
-    typeof nextConfig === "function"
-      ? nextConfig(phase, thing)
-      : nextConfig;
-  return internalConfigObj;
+  let internalConfigObj = typeof nextConfig === 'function' ? nextConfig(phase, ...thing) : nextConfig
+  return internalConfigObj
 }
 
-module.exports = withPrismaPlugin
+export default withPrismaPlugin
--- src/webpack-plugin.ts
+++ src/webpack-plugin.ts
@@ -3,19 +3,19 @@ import { Compiler } from 'webpack'
 
 const PLUGIN_NAME = 'PrismaClientReloaderWebpackPlugin'
 
-const PATHS = ['.prisma/client/index.js', '@prisma/client/index.js', '@prisma/client/runtime/index.js']
-const TESTS = [
+const PRISMA_PATHS = ['.prisma/client/index.js', '@prisma/client/index.js', '@prisma/client/runtime/index.js']
+const PRISMA_REGEX_TESTS = [
   /\.prisma\/client\/index.js/,
   /@prisma\/client\/index.js/,
   /@prisma\/client\/runtime\/index.js/,
 ]
 class PrismaClientReloaderWebpackPlugin {
   apply(compiler: Compiler) {
-    const clientPaths = PATHS.map(p => path.join(compiler.context, 'node_modules', p))
+    const clientPaths = PRISMA_PATHS.map((p) => path.join(compiler.context, 'node_modules', p))
     compiler.hooks.afterEmit.tapAsync(PLUGIN_NAME, (compilation, callback) => {
       const moduleIds = Object.keys(require.cache)
-      TESTS.forEach(regex => {
-        moduleIds.forEach(moduleId => {
+      PRISMA_REGEX_TESTS.forEach((regex) => {
+        moduleIds.forEach((moduleId) => {
           if (regex.test(moduleId)) {
             // console.log(moduleId)
             delete require.cache[moduleId]
@@ -25,15 +25,15 @@ class PrismaClientReloaderWebpackPlugin {
       callback()
     })
 
-    compiler.hooks.compilation.tap(PLUGIN_NAME, compilation => {
+    compiler.hooks.compilation.tap(PLUGIN_NAME, (compilation) => {
       if (compilation.fileDependencies) {
-        clientPaths.forEach(p => {
+        clientPaths.forEach((p) => {
           compilation.fileDependencies.add(p)
         })
       }
       // @ts-ignore
       else if (compilation.compilationDependencies) {
-        clientPaths.forEach(p => {
+        clientPaths.forEach((p) => {
           // @ts-ignore
           compilation.compilationDependencies.add(p)
         })
